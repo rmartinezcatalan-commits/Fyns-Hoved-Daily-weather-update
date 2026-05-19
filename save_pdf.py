ad5e56f3b44ea07654b3c6f1bad2eb9f7b1034b9
rmartinezcatalan-commits <rmartinezcatalan@gmail.com>
2026-05-19 09:49:50 +0200

    Update save_pdf.py

--- save_pdf.py
+++ save_pdf.py
@@ -1,6 +1,5 @@
 from playwright.sync_api import sync_playwright
 from datetime import datetime
-from pathlib import Path
 import os
 
 today = datetime.now().strftime("%Y%m%d")
@@ -12,10 +11,11 @@ pdf_path = f"pdfs/{today}.pdf"
 url = "https://www.windfinder.com/forecast/fyn_nordskov"
 
 with sync_playwright() as p:
- browser = p.chromium.launch(
-    headless=True,
-    args=["--no-sandbox", "--disable-setuid-sandbox"]
-)
+    browser = p.chromium.launch(
+        headless=True,
+        args=["--no-sandbox", "--disable-setuid-sandbox"]
+    )
+
     page = browser.new_page()
 
     page.goto(url, wait_until="networkidle")
